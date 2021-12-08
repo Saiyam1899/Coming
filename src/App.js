f854b74cd06976748398734b0c426b4fb13b07bd
Saiyam1899 <95686190+Saiyam1899@users.noreply.github.com>
2021-12-08 14:02:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import ReactDOM from "react-dom";
 import Preloader from "./Components/Preloader/Preloader";
 import Timer from "./Components/Countdown/Timer";
 import Optin from "./Components/Optin/Optin";
+import Section from "./Components/Section/index";
 import "./App.css";
 import logo from "./logo-01.png";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h1 color="white">Website Coming Soon</h1>
-        <img src={logo} className="logo"/>
-
-      
-      </div>
+      <Section />
     </div>
   );
 }
